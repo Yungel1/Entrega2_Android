b49f2769efbb20956f8086a9c0c96cb7f27f25b5
Adrián Sáanchez <adriansanchez0123@gmail.com>
2022-03-05 14:09:32 +0100

    layout login

---
+++ app/src/main/java/com/example/entrega1/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.entrega1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void onClickEntrar(View v){
+
+    }
+
+    public void onClickIdioma(View v){
+
+    }
+
+    public void onClickRegistrate(View v){
+
+    }
+
+}
